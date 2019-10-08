861ded7c1aa71e2a1b4c7cff55e06b3cfe955526
kanwal bhatia <kanbhatia@hotmail.com>
2019-10-07 20:16:25 -0400

    -

--- static/GeoMap.js
+++ static/GeoMap.js
@@ -4,15 +4,25 @@ d3.select(".panel-title").text(Panel_title);
 console.log(Panel_title);
 
 function getJsonData(data) {
-  // console.log(Object.values(data.Postcode))
+  data.Year = Object.entries(data.Year).map(([key, value]) => (value));
+  data.Neighbourhood = Object.entries(data.Neighbourhood).map(([key, value]) => (value));
+  data.Total = Object.entries(data.Total).map(([key, value]) =>{
+    if (value === "**"){
+      return 1;
+    }
+    else{
+      return (value)
+    }
+  });
+  data.Whole_Toronto_AV = Object.entries(data.Whole_Toronto_AV).map(([key, value]) => (value));
 
-  // console.log(subjects)
-  return data;
+  map_data = [data.Year,data.Neighbourhood,data.Total,data.Whole_Toronto_AV]
+  return map_data;
 }
 
 // links
-var appstat = d3.json("http://localhost:5000/appstat").then(getJsonData);
-var rentavg = d3.json("http://localhost:5000/rentavg").then(getJsonData);
+// var appstat = d3.json("http://localhost:5000/appstat").then(getJsonData);
+// var rentavg = d3.json("http://localhost:5000/rentavg").then(getJsonData);
 var allrent = d3.json("http://localhost:5000/allrent").then(getJsonData);
 
 // Creating map object
@@ -30,22 +40,25 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(map);
 
 // Getting Geojson data
-var link_GeoJson = "./static/Data/Neighbourhoods.geojson" 
+var link_GeoJson = "./static/Data/Neighbourhoods.geojson"
 
-
-function chooseColor(neighborhood){
-  name = neighborhood.properties.AREA_NAME;
+function chooseColor(name) {
   New_name = name.split(" (")[0]
-  x = "red"
-  allrent.then((data)=> {
-    var neighborhood_name = Object.values(data)[6];
-    var year = Object.values(data)[12];
-
-    console.log(neighborhood_name)
-  })
-  // console.log(New_name)
-  return x
-
+  
+    switch (New_name) {
+    case "New Toronto":
+      return "yellow";
+    case "Bronx":
+      return "red";
+    case "Manhattan":
+      return "orange";
+    case "Queens":
+      return "green";
+    case "Staten Island":
+      return "purple";
+    default:
+      return "black";
+    }
 }
 
 d3.json(link_GeoJson).then(data => {
@@ -53,36 +66,35 @@ d3.json(link_GeoJson).then(data => {
   // Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
     // Style each feature (in this case a neighborhood)
-    style: function(neighborhood) {
+    style: function (neighborhood) {
       return {
         color: "white",
         // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-
-        fillColor: chooseColor(neighborhood),
+        fillColor: chooseColor(neighborhood.properties.AREA_NAME),
         fillOpacity: 0.5,
         weight: 1.5
       };
     },
     // Called on each feature
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function (feature, layer) {
       // Set mouse events to change map styling
       layer.on({
         // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-        mouseover: function(event) {
+        mouseover: function (event) {
           layer = event.target;
           layer.setStyle({
             fillOpacity: 0.9
           });
         },
         // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-        mouseout: function(event) {
+        mouseout: function (event) {
           layer = event.target;
           layer.setStyle({
             fillOpacity: 0.5
           });
         },
         // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
-        click: function(event) {
+        click: function (event) {
           map.fitBounds(event.target.getBounds());
         }
       });
